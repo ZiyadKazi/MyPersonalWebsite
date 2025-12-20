31f7c815c95c2daa7ecff394127fbb77ce09e673
Ziyad Kazi <ziyad.kazi@gmail.com>
2025-12-20 10:28:34 -0500

    Have to fix up the burger menu transition and for it to show up when clicking on hamburger button

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,20 @@
 import './NavBar.css'
+import Burger from './Burger.jsx'
 import { Link } from 'react-router-dom';
+import { useState } from 'react'
+
 
 const NavBar = () => {
     const logobeg = "</"
     const logomid = "Ziyad"
     const logoend = ">"
     const logo = logobeg + logomid + logoend
+    const [open, setOpen] = useState(false)
+
+    const toggle = () => {
+        setOpen(!open)
+    }
+
     return (
         <div className="nav-bar">
             <div className="logo">
@@ -20,7 +29,11 @@ const NavBar = () => {
                 </div>
             </div>
 
-            <ul className="nav-links">
+            <div>
+                <Burger isOpen = {open} toggleMenu = {toggle} />
+            </div>
+
+            <ul className={`nav-links ${open ? 'open' : ''} `}>
                 <li className="nav-link"><Link to="/">Home</Link></li>
                 <li className="nav-link"><Link to="/about">About</Link></li>
                 <li className="nav-link"><Link to="/contact">Contact</Link></li>
